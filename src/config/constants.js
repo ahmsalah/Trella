eb47f5dad9f160a2a53c9ccfd0a74b46fa0f24d8
ahmsalah <aemdsalah@gmail.com>
2021-02-12 17:15:12 +0200

    Add Cairo coordinates as initial location

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,9 @@
-// eslint-disable-next-line import/prefer-default-export
 export const isIOS =
   /iPad|iPhone|iPod/.test(navigator.platform) ||
   (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1 && !window.MSStream);
+
+// Cairo coordinates
+export const initialLocation = {
+  lat: 30.04735861255239,
+  lng: 31.235888303861767,
+};
