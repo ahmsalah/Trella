bd2eeeabe0844c968659b451ef21b76bf605c595
ahmsalah <aemdsalah@gmail.com>
2021-02-12 16:37:25 +0200

    Add StatCard component

---
+++ src/components/StatCard/StatCard.js
@@ -0,0 +1,54 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+import useStyles from './styles';
+
+function StatCard({ title, icon, value, onClick, variant, className }) {
+  const isHorizontal = variant === 'horizontal';
+  const classes = useStyles({ isHorizontal });
+
+  const BtnWrapper = childs =>
+    onClick ? (
+      <ButtonBase className={classes.buttonBase} onClick={onClick}>
+        {childs}
+      </ButtonBase>
+    ) : (
+      childs
+    );
+
+  return BtnWrapper(
+    <Card className={clsx(classes.root, !!onClick && classes.cardButton, className, 'stat-card')}>
+      <div className={classes.titleContainer}>
+        {icon}
+        <Typography variant="body2" align="center">
+          {title}
+        </Typography>
+      </div>
+      <Typography
+        variant="h6"
+        align="center"
+        className={clsx(classes.stat, !isHorizontal && classes.fontSize)}
+      >
+        <b>{value}</b>
+      </Typography>
+    </Card>,
+  );
+}
+
+StatCard.defaultProps = {
+  variant: 'vertical',
+};
+
+StatCard.propTypes = {
+  variant: PropTypes.oneOf(['vertical', 'horizontal']),
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.node.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.string]).isRequired,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+};
+
+export default memo(StatCard);
---
+++ src/components/StatCard/styles.js
@@ -0,0 +1,61 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(
+  ({
+    spacing,
+    breakpoints,
+    shadows,
+    mixins,
+    palette: { borderColor },
+    shape: { borderRadius },
+  }) => ({
+    root: {
+      flex: 1,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      padding: ({ isHorizontal }) => (isHorizontal ? spacing(1, 2, 1.5) : spacing(1.5, 1.5, 1)),
+      minWidth: 92,
+      border: '1px solid',
+      borderColor,
+      boxShadow: mixins.boxShadows.primary.light,
+    },
+    buttonBase: {
+      borderRadius,
+      width: '100%',
+    },
+    cardButton: {
+      '&&&': {
+        margin: 0,
+      },
+      '&:hover': {
+        boxShadow: shadows[5],
+      },
+    },
+    stat: {
+      order: ({ isHorizontal }) => (isHorizontal ? 1 : 3),
+      width: '100%',
+      textOverflow: 'ellipsis',
+      overflowWrap: 'anywhere',
+    },
+    fontSize: {
+      [breakpoints.up('md')]: {
+        fontSize: '1.75rem',
+      },
+      [breakpoints.up('sm')]: {
+        fontSize: '1.5rem',
+      },
+    },
+    titleContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      order: ({ isHorizontal }) => (isHorizontal ? 3 : 1),
+      flexDirection: ({ isHorizontal }) => (isHorizontal ? 'row' : 'column'),
+      '& .MuiIcon-root, & .MuiSvgIcon-root': {
+        fontSize: ({ isHorizontal }) => (isHorizontal ? '2.4rem' : '2.7rem'),
+        margin: ({ isHorizontal }) => (isHorizontal ? spacing(0, 1, 0, 0) : spacing(0, 0, 0.5)),
+      },
+    },
+  }),
+);
