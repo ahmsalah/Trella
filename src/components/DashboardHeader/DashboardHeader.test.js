7e96a3cde44a3a584993f812842298dd11a35e7c
ahmsalah <aemdsalah@gmail.com>
2021-02-13 20:51:14 +0200

    Add DashboardHeader test

---
+++ src/components/DashboardHeader/DashboardHeader.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import DashboardHeader from './DashboardHeader';
+
+test('filteredView is set to true and renders address', () => {
+  render(<DashboardHeader loading={false} filteredView address="Cairo" />);
+  screen.getByRole('button', { name: /All Shipments/i });
+  screen.getByRole('button', { name: /Change Location/i });
+  screen.debug();
+
+  expect(screen.getByText(/Viewing shipments near/i)).toHaveTextContent('Cairo');
+});
+
+test('Location button should be disabled while loading', () => {
+  render(<DashboardHeader loading filteredView={false} address="Cairo" />);
+
+  expect(screen.getByRole('button', { name: /Location/i })).toBeDisabled();
+});
